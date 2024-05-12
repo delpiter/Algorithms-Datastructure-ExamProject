f4bb32008009574bb8d8606e634d9e7ae6532109
delpiter <gioele.foschi@gmail.com>
2024-05-12 18:09:36 +0200

    minor changes

--- 0001122551.c
+++ 0001122551.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 #include <math.h>
+#include <limits.h>
 
 /*Priority Queue Structure-Min-Heap*/
 typedef struct
@@ -24,7 +25,7 @@ typedef struct Cell
 {
     int height;
     char visited;
-    int height_difference;
+    int shortest_dist_from_origin;
 } Cell;
 
 /*Matrix that contains all the necessary data*/
@@ -36,6 +37,13 @@ typedef struct Matrix
     int m; /*Number of Columns*/
     Cell ***Matrix;
 } Matrix;
+
+/*Data structure for the solution*/
+typedef struct Solution
+{
+    int *best_path[2];
+    long int path_cost;
+} Solution;
 
 /*Grapgh Structure-Adjacency List*/
 
@@ -65,7 +73,13 @@ typedef struct
     int *out_deg; /* grado uscente dei nodi       */
 } Graph;
 
-/*Test Main*/
+/*
+ * Function used to test the program in debug mode
+ *
+ * --------------------------
+ *
+ * There are no parameters in input, the input file is forced inside the code
+ */
 /*int main()
 {
     char* file_name = "test0.in";
@@ -73,7 +87,7 @@ typedef struct
     int a;
     FILE *pFile;
 
-    printf("ciao");
+    printf("Hello, World!");
 
     memLeakTest[0] = 5;
     a = memLeakTest[0];
@@ -81,6 +95,9 @@ typedef struct
 
 }*/
 
+/*
+ * Initializes a Matrix structure
+ */
 Matrix *matrix_init()
 {
     Matrix *M;
@@ -92,6 +109,13 @@ Matrix *matrix_init()
     return M;
 }
 
+/*
+ * Frees all the memory allocated of a given matrix structure
+ *
+ * --------------------------
+ *
+ * M -> Pointer to the matrix
+ */
 void matrix_free(Matrix *M)
 {
     int i, j;
@@ -108,6 +132,15 @@ void matrix_free(Matrix *M)
     free(M);
 }
 
+/*
+ * Reads data from a file stream and populates a Matrix structure.
+ * The file in input has to be in the right format
+ *
+ * --------------------------
+ *
+ * filein -> Pointer to the input file stream.
+ * M      -> Pointer to the Matrix structure to be populated.
+ */
 void read_input_file(FILE *filein, Matrix *M)
 {
     int i, j;
@@ -126,12 +159,15 @@ void read_input_file(FILE *filein, Matrix *M)
             M->Matrix[i][j] = (Cell *)malloc(sizeof(Cell)); /*Allocate Memory for each cell of the Matrix*/
 
             fscanf(filein, "%d", &(M->Matrix[i][j]->height));
-            M->Matrix[i][j]->height_difference = -1;
+            M->Matrix[i][j]->shortest_dist_from_origin = INT_MAX;
             M->Matrix[i][j]->visited = 0;
         }
     }
 }
 
+/*
+ * Prints the contents of the matrix structure
+ */
 void print_matrix(Matrix *M)
 {
     int i, j;
@@ -145,23 +181,34 @@ void print_matrix(Matrix *M)
     {
         for (j = 0; j < M->m; j++)
         {
-            fprintf(stdout, "%d\t", M->Matrix[i][j]->height);
+            fprintf(stdout, "%d|\t", M->Matrix[i][j]->height);
         }
         fprintf(stdout, "\n");
     }
 }
 
+Solution *Dijkstra(Matrix *M)
+{
+    Solution *sol;
+
+    return sol;
+}
+
 int main(int argc, char *argv[])
 {
     FILE *filein;
+
+    /*The Matrix used to calculate the best path*/
     Matrix *M = matrix_init();
 
+    /*Input File Check*/
     if (argc != 2)
     {
         fprintf(stderr, "Input Syntax Error");
         return EXIT_FAILURE;
     }
 
+    /*Open input File*/
     if (strcmp(argv[1], "-") != 0)
     {
         filein = fopen(argv[1], "r");
@@ -175,8 +222,10 @@ int main(int argc, char *argv[])
     read_input_file(filein, M);
 
     fclose(filein);
+
     print_matrix(M);
 
     matrix_free(M);
+
     return EXIT_SUCCESS;
 }
